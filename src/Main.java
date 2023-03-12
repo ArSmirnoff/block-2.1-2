adf9880d928e42c7fc6464369fed10ed0b6a6006
ArSmirnoff <Teon-cm@yandex.ru>
2023-03-12 21:14:08 +0300

    change src

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,15 @@
 public class Main {
     public static void main(String[] args) {
-        int balance = 200; //начальный счет
-        int amountDeposit = 1100; //сумма пополнения
+        int startingBalance = 200; //начальный баланс
+        int amountDeposit = 200; //сумма пополнения
         int bonus; //бонус
 
         if (amountDeposit > 1000) {
-            bonus= amountDeposit / 100;
-        } else  {
-            bonus= 0;
+            bonus = amountDeposit / 100;
+        } else {
+            bonus = 0;
         }
-        int z = amountDeposit + balance + bonus;
-        System.out.println("На вашем счету:" + z + "руб");
+        int balance = amountDeposit + startingBalance + bonus; //баланс конечный
+        System.out.println("На вашем счету:" + balance + "руб");
     }
 }
